aef7551ca399d076e2e29fb2c8fd923494af8a9f
lexach91 <ronmctailor@protonmail.com>
2022-01-27 20:26:41 +0800

    Add function to get random quote

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 /* jshint esversion: 6 */
+<<<<<<< HEAD
+=======
+const jokeText = document.querySelector(".joke-text");
+const activityText = document.querySelector(".activity-text");
+const quoteText = document.querySelector(".quote-text");
+const jokeButton = document.querySelector(".joke-button");
+const activityButton = document.querySelector(".activity-button");
+const quoteButton = document.querySelector(".quote-button");
+
+
+>>>>>>> ec89339 (Add function to get random quote)
 const getRandomJoke = () => {
   fetch(
     "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit",
@@ -17,6 +28,7 @@ const getRandomJoke = () => {
 };
 
 const getRandomActivity = () => {
+<<<<<<< HEAD
   fetch("https://www.boredapi.com/api/activity/", {
     method: "GET",
   })
@@ -35,3 +47,27 @@ const getRandomQuote = () => {
     //   quoteText.innerHTML = data.content + "<br>" + data.author;
     });
 };
+=======
+    fetch("http://www.boredapi.com/api/activity/", {
+        method: "GET",
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            activityText.innerHTML = data.activity;
+        });
+};
+
+const getRandomQuote = () => {
+    fetch("https://api.quotable.io/random", {
+        method: "GET",
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            quoteText.innerHTML = data.content + "<br>" + data.author;
+        });
+};
+
+jokeButton.addEventListener("click", getRandomJoke);
+activityButton.addEventListener("click", getRandomActivity);
+quoteButton.addEventListener("click", getRandomQuote);
+>>>>>>> ec89339 (Add function to get random quote)
