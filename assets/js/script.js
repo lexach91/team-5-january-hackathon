890775915a9f6bedefd2b0a6374d44b31d0f3cc3
lexach91 <ronmctailor@protonmail.com>
2022-01-28 13:15:01 +0800

    Add loading animation while fetching data

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const refreshQuote = document.querySelector("#refresh-quote");
   // Functions to get random jokes/activities/quotes
   const getRandomJoke = () => {
+      let icon = refreshJoke.querySelector("i");
+        icon.classList.add("fa-spin");
     fetch(
       "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit",
       {
@@ -24,26 +26,43 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
           jokeContainer.innerHTML = `<p>${data.setup}</p><p>${data.delivery}</p>`;
         }
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        icon.classList.remove("fa-spin");
       });
   };
 
   const getRandomActivity = () => {
+    let icon = refreshActivity.querySelector("i");
+    icon.classList.add("fa-spin");
     fetch("https://www.boredapi.com/api/activity/", {
       method: "GET",
     })
       .then((res) => res.json())
       .then((data) => {
         activityContainer.innerHTML = `<p>${data.activity}</p>`;
-      });
+      })
+        .catch((err) => console.log(err))
+        .finally(() => {
+            icon.classList.remove("fa-spin");
+        });
+      
   };
 
   const getRandomQuote = () => {
+    let icon = refreshQuote.querySelector("i");
+    icon.classList.add("fa-spin");
     fetch("https://api.quotable.io/random", {
       method: "GET",
     })
       .then((res) => res.json())
       .then((data) => {
         quoteContainer.innerHTML = `<p>${data.content}</p><p><em>${data.author}<em></p>`;
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        icon.classList.remove("fa-spin");
       });
   };
 
